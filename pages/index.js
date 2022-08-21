c0b7eb8c891d28c403c8900bfcefea672f2870c2
N-hotate <sadalsuud.lowenzahn317@gmail.com>
2022-08-21 22:40:38 +0900

    Next.js の静的ファイル出力設定を追加

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {/* ようこそ <a href="https://nextjs.org">Next.js!</a> */}
         </h1>
 
         <p className={styles.description}>
@@ -60,7 +61,8 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
