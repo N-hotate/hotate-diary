1ea8d82c2b704a1f8d6ad0ac3e411e9e0a8489fe
N-hotate <sadalsuud.lowenzahn317@gmail.com>
2022-09-06 20:25:43 +0900

    hotate-diary を実装

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,3 +24,5 @@ const app = initializeApp(firebaseConfig);
 if(typeof window !== 'undefined'){
   const analytics = getAnalytics(app);
 }
+
+export const db = getFirestore(); // 他のファイルからも呼び出せるように、db をexport しておく
